1a58113ba81c7fcbd1bdf0c485c0016d9af4090e
Juangr4 <jgarciaruiz2001@gmail.com>
2022-11-26 14:17:03 +0100

    Feature 7: Completed

--- mysite/gamingworld/views.py
+++ mysite/gamingworld/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
+from random import sample
 
 
 def index(request):
-    return render(request, 'gamingworld/index.html')
+    return render(request, 'gamingworld/index.html', {"slider": enumerate(sample(list(Producto.objects.all()), 3))})
 
 def products(request):
     productos_todos = Producto.objects.all()
