617c4abb02761ef444171a33a519d859a0350dd6
josconsan1 <josconsan1@alum.us.es>
2022-11-26 20:23:08 +0100

    Feature 15: complete

--- mysite/gamingworld/urls.py
+++ mysite/gamingworld/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('products/', views.products, name='products'),
     path('products/<int:product_id>/', views.detail, name='product_details'),
+    path('shipping_polite/', views.shipping_polite, name='shipping_polite'),
     path('admin/', admin.site.urls),
 ]
--- mysite/gamingworld/views.py
+++ mysite/gamingworld/views.py
@@ -12,6 +12,7 @@ def products(request):
     secciones = Seccion.objects.all()
     fabricantes = Fabricante.objects.all()
 
+    nombre_de_producto_buscado = request.GET.get("name")
     genero_buscado = request.GET.get("producttype")
     seccion_buscada = request.GET.get("section")
     fabricante_buscado = request.GET.get("manufacturer")
@@ -21,19 +22,24 @@ def products(request):
         genero_valido = True
         seccion_valida = True
         fabricante_valido = True
+        nombre_de_producto_buscado_valido = True
+
+        if nombre_de_producto_buscado is not None:
+            nombre_de_producto_buscado_valido = producto.nombre.lower() == nombre_de_producto_buscado.replace("+"," ").lower()
         if genero_buscado is not None:
             genero_valido = producto.genero == Genero.objects.get(id__exact = genero_buscado)
         if seccion_buscada is not None:
             seccion_valida = producto.seccion == Seccion.objects.get(id__exact = seccion_buscada)
         if fabricante_buscado is not None:
             fabricante_valido = producto.fabricante == Fabricante.objects.get(id__exact = fabricante_buscado)
-        if genero_valido and seccion_valida and fabricante_valido:
+        if genero_valido and seccion_valida and fabricante_valido and nombre_de_producto_buscado_valido:
             productos.append(producto)
             
-
+    
     lista_tuplas_productos = []
     for i in range(0, len(productos), 3):
         lista_tuplas_productos.append(tuple(productos[i:i+3]))
+        
     modelmap = {'trios_productos':lista_tuplas_productos, 
                 'generos':generos,
                 'secciones':secciones,
@@ -44,3 +50,6 @@ def detail(request, product_id):
     producto = Producto.objects.get(id__exact=product_id)
     modelmap = {'producto':producto}
     return render(request, 'gamingworld/product_details.html', modelmap)
+
+def shipping_polite(request):
+    return render(request, 'gamingworld/shipping_polite.html')
