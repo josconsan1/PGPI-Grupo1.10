51d8a13c0032af830942af6824a88b605b65af64
josconsan1 <josconsan1@alum.us.es>
2022-11-27 11:25:08 +0100

    feature-16: Done

--- mysite/gamingworld/urls.py
+++ mysite/gamingworld/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('products/', views.products, name='products'),
     path('products/<int:product_id>/', views.detail, name='product_details'),
+    path('return_policy/',views.return_policy, name='return_policy'),
     path('admin/', admin.site.urls),
 ]
