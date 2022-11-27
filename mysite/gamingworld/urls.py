168e4a48195a3fe19251c5d547d597a84296fc3a
Pablo <pabesphoy@alum.us.es>
2022-11-27 23:34:55 +0100

    Feature 11: Done

--- mysite/gamingworld/urls.py
+++ mysite/gamingworld/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from django.contrib import admin
-
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('products/', views.products, name='products'),
     path('products/<int:product_id>/', views.detail, name='product_details'),
+    path('cart/', views.cart, name='cart'),
+    path('addproduct/<int:product_id>/', views.add_product_to_cart, name='addproduct'),
+    path('deleteproduct/<int:product_id>/', views.delete_product_from_cart, name='deleteproduct'),
     path('admin/', admin.site.urls),
 ]
--- mysite/gamingworld/views.py
+++ mysite/gamingworld/views.py
@@ -1,13 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
+from datetime import *
+from random import randint
 
 
 def index(request):
     return render(request, 'gamingworld/index.html')
 
 def products(request):
-    productos_todos = Producto.objects.all()
+    
+    print(request.COOKIES.items())
     generos = Genero.objects.all()
     secciones = Seccion.objects.all()
     fabricantes = Fabricante.objects.all()
@@ -16,7 +19,75 @@ def products(request):
     seccion_buscada = request.GET.get("section")
     fabricante_buscado = request.GET.get("manufacturer")
 
+    lista_tuplas_productos = get_products_by_tuples(genero_buscado, seccion_buscada, fabricante_buscado)
+    modelmap = {'trios_productos':lista_tuplas_productos, 
+                'generos':generos,
+                'secciones':secciones,
+                'fabricantes':fabricantes}
+    return render(request, 'gamingworld/products.html', modelmap)
+
+def detail(request, product_id):
+    producto = Producto.objects.get(id__exact=product_id)
+    modelmap = {'producto':producto}
+    response = render(request, 'gamingworld/product_details.html', modelmap)
+    return response
+
+def cart(request):
+    productos_amounts = get_products_from_cookies(request)
+    modelmap = {"products_amounts" : productos_amounts}
+    response = render(request, 'gamingworld/cart.html', modelmap)
+    return response
+
+def add_product_to_cart(request, product_id):
+    productos_amounts = get_products_from_cookies(request)
+    modelmap = {'trios_productos':get_products_by_tuples(), "products_amounts" : productos_amounts}
+    amount = request.GET.get("amount")
+    if amount == '':
+        modelmap["message"] = "No has añadido ningún artículo"
+        response = render(request, 'gamingworld/cart.html', modelmap)
+        return response
+    key = "product_id_"+str(product_id)
+    value = request.COOKIES.get(key)
+    if value is None:
+        value = 0
+    
+    if int(amount) + int(value) <= Producto.objects.get(id__exact=product_id).stock:
+        modelmap["message"] = str(int(amount) + int(value)) + " artículos añadidos"
+        response = render(request, 'gamingworld/products.html', modelmap)
+        response.set_cookie(key, str(int(amount) + int(value)))
+        return response
+    else:
+        modelmap["error_message"] = "Ha sobrepasado la cantidad de unidades disponibles de este artículo"
+        response = render(request, 'gamingworld/cart.html', modelmap)
+        return response
+
+    
+
+def delete_product_from_cart(request, product_id):
+    key = "product_id_"+str(product_id)
+    productos_amounts = get_products_from_cookies(request)
+    modelmap = {'trios_productos':get_products_by_tuples(), "products_amounts" : productos_amounts}
+    response = render(request, 'gamingworld/products.html',modelmap)
+    response.delete_cookie(key)
+
+    
+    return response
+
+
+def get_products_from_cookies(request):
+    productos_amounts = []
+    cookies = request.COOKIES
+    for cookie in cookies.keys():
+        if cookie[0:-1] == "product_id_":
+            producto = Producto.objects.get(id__exact=cookie[-1])
+            productos_amounts.append(tuple((producto, cookies[cookie])))
+    print(productos_amounts)
+    return productos_amounts
+
+
+def get_products_by_tuples(genero_buscado=None, seccion_buscada=None, fabricante_buscado=None):
     productos = []
+    productos_todos = Producto.objects.all()
     for producto in productos_todos:
         genero_valido = True
         seccion_valida = True
@@ -34,13 +105,4 @@ def products(request):
     lista_tuplas_productos = []
     for i in range(0, len(productos), 3):
         lista_tuplas_productos.append(tuple(productos[i:i+3]))
-    modelmap = {'trios_productos':lista_tuplas_productos, 
-                'generos':generos,
-                'secciones':secciones,
-                'fabricantes':fabricantes}
-    return render(request, 'gamingworld/products.html', modelmap)
-
-def detail(request, product_id):
-    producto = Producto.objects.get(id__exact=product_id)
-    modelmap = {'producto':producto}
-    return render(request, 'gamingworld/product_details.html', modelmap)
+    return lista_tuplas_productos
